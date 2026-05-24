c7d04f8332b488ec7ad72bb5da0ad2143ca4bb82
Siddarth2230 <112377243+Siddarth2230@users.noreply.github.com>
2026-05-24 13:29:38 +0530

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        mp = defaultdict(int)
+
+        for i in range(len(numbers)):
+            rem = target - numbers[i]
+
+            if rem in mp:
+                return [mp[rem], i+1]
+            
+            mp[numbers[i]] = i+1
+        
+        return []
+
+
+        
